10ef4012317143ad356fb064e8dc5aa1fa44118c
Kiyoshi Yamashita <53377829+kirinboy96@users.noreply.github.com>
2020-12-30 16:41:04 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 2;
+        int total = CalcLogic.tasu(a, b);
+        int delta = CalcLogic.hiku(a, b);
+        System.out.println("足すと" + total + "、引くと" + delta);
+    }
+}
